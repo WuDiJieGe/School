4c60360e236a02d0bcd565a33cf089192ed1c57a
WuDiJieGe <54763272+WuDiJieGe@users.noreply.github.com>
2020-05-15 16:08:58 +0800

    Update Work_2020_5_15.java

--- Work_2020_5_15.java
+++ Work_2020_5_15.java
@@ -6,45 +6,28 @@ public class Work_2020_5_15 {
 //		秋季：9 10 11
 //		冬季： 12 1 2
 		
-		int number = 11;
+		int number = 9;
 		switch (number) {
+		case 12:
+		case 1:
+		case 2:
+			System.out.println(number+"是冬季");
+			break;
 		case 3:
-			System.out.println(number+"是春季");
-			break;
 		case 4:
-			System.out.println(number+"是春季");
-			break;
 		case 5:
-			System.out.println(number+"是春季");
+			System.out.println(number+"是夏季");
 			break;
 		case 6:
-			System.out.println(number+"是夏季");
-			break;
 		case 7:
-			System.out.println(number+"是夏季");
-			break;
 		case 8:
 			System.out.println(number+"是夏季");
 			break;
 		case 9:
-			System.out.println(number+"是秋季");
-			break;
 		case 10:
-			System.out.println(number+"是秋季");
-			break;
 		case 11:
 			System.out.println(number+"是秋季");
 			break;
-		case 12:
-			System.out.println(number+"是冬季");
-			break;
-		case 1:
-			System.out.println(number+"是冬季");
-			break;
-		case 2:
-			System.out.println(number+"是冬季");
-			break;
-			
 		default:
 			System.out.println("请输入一个正常的月份");
 			break;
